91392a77ebeecde43ce2a211d374dd583f11f90d
nihitx <masnadhossain@live.com>
2015-12-05 19:36:59 +0200

    Put cities to home controller

--- app/components/home/app.home.js
+++ app/components/home/app.home.js
@@ -1,11 +1,16 @@
 // Use this file to setup application submodule and load its dependencies
 // Optinally the following files can be created and added as dependencies: 'app.home.routes', 'app.home.core', 'app.home.services', 'app.home.config'
-angular.module('app.home', ['app.home.controller'])
+angular.module('app.home', ['app.home.controller', 'app.cities.service'])
 
 // Setup routes
 .config(['$routeProvider', function ($routeProvider) {
   $routeProvider.when('/home', {
     templateUrl: 'app/components/home/home.tpl.html',
-    controller: 'HomeController as home'
+    controller: 'HomeController as home',
+    resolve:{
+      cities: ['Cities', function (Cities) {
+        return Cities.query();
+      }]
+    }
   });
 }]);
--- app/components/home/home.ctrl.js
+++ app/components/home/home.ctrl.js
@@ -2,6 +2,7 @@
 angular.module('app.home.controller', [])
 
 // Define home controller
-.controller('HomeController', ['$scope', function HomeController($scope) {
+.controller('HomeController', ['$scope', 'cities', function HomeController($scope, cities) {
   $scope.greeting = "Welcome to an AngularJS application structure";
+  $scope.countries = cities;
 }]);
